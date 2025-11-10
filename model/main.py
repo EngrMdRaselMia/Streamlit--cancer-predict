3b73db8550a941d9767de1364f762aba5b92a795
MD Rasel Mia <raselipe2016@gmail.com>
2025-11-10 21:40:17 +0600

    UI update: reduced top spacing, updated CSS enhancements

--- model/main.py
+++ model/main.py
@@ -27,7 +27,7 @@ def create_model(data):
     return model, scaler
 
 def read_cleaned_data():
-    data = pd.read_csv(r"C:\Users\MD Rasel Mia\OneDrive\Desktop\archive\data\data.csv")
+    data = pd.read_csv(r"data\data.csv")
     data = data.drop(['Unnamed: 32', 'id'], axis=1)
     data['diagnosis'] = data['diagnosis'].map({'M': 1, 'B': 0})
     return data
